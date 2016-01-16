70533617bf81bf13b91f61a711f126f27cd407b1
vicadi <vicadi7@gmail.com>
2016-01-15 23:42:13 -0500

    archivo instagram.rb

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,9 @@
+require "instagram"
+
+    Instagram.configure do |config|
+
+    config.client_id = "50c3b1af58b846278e6f2540d34f3f03"
+
+    config.access_token = "1792475136.50c3b1a.4dc4538b87b148a1bf5d25503fe12adc"
+
+    end
